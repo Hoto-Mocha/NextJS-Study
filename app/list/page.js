34f0b8bfc63ccf8cd738f3d0bb665db979233b51
Hoto-Mocha <mtj2010@naver.com>
2025-01-31 01:08:04 +0900

    Routing 예제 구현

---
+++ app/list/page.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function List() {
+    return (
+        <div>
+            <h4 className="title-sub">상품목록</h4>
+            <div className="food">
+                <h4>상품 1 $40</h4>
+            </div>
+            <div className="food">
+                <h4>상품 2 $40</h4>
+            </div>
+        </div>
+    )
+}
